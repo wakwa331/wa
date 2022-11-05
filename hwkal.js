0aa8e0b024c823a3eff95f8002ed873ef1dd7e68
wakwa331 <pinobagas03@gmail.com>
2022-11-05 18:05:59 +0700

    Made in Pino

---
+++ hwkal.js
@@ -0,0 +1,71 @@
+// Buat Lu Yang Jual Sc Ini Yang Jujur Jangan Samp Nipu Apalagi Lari Dari Tanggung Jawab
+
+// Base Haikal Hw Mods : Copyan Dari Base Dika
+
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+global.wlcm = []
+global.ntnsfw = []
+global.ntnsfww = []
+
+//gausah di apa² in!
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+//=================================================//
+//—————「 Set Kebutuhan Button & Kontak 」—————//
+//ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
+global.youtube = 'https://pinohost.my.id' // ubah aia
+global.ig = 'https://instagram.com/pinomdstore' // ubah aja
+global.email = 'admin@pinohost.my.id'
+global.region = 'indonesia'
+//—————「 Set Nama Own & Bot 」—————//
+global.ownername = '𝙿𝙸𝙽𝚉 𝙼𝙰𝙺𝙴𝚁' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = '𝙿𝙸𝙽𝚉 𝙼𝙰𝙺𝙴𝚁' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = '𝙿𝙸𝙽𝚉 𝙼𝙰𝙺𝙴𝚁' //ubah jadi nama mu, note tanda ' gausah di hapus!
+//=================================================//
+// Other
+global.owner = ['628891753051'] // ubah aja pake nomor lu
+global.premium = ['628891753051'] // ubah aja pake nomor lu
+global.packname = '𝙿𝙸𝙽𝚉 𝙼𝙰𝙺𝙴𝚁' // ubah aja
+global.ownerr = ['𝙿𝙸𝙽𝚉 𝙼𝙰𝙺𝙴𝚁'] // ubaha aja
+global.author = '𝙿𝙸𝙽𝚉 𝙼𝙰𝙺𝙴𝚁' //ubah aja
+global.sessionName = './sessionye/session' //Gausaj Di Ubah
+global.prefa = ['','!','.',',','🐤','🗿'] // Gausah Mendingan 
+global.sp = '⭔' // Gausah Juga
+
+// Terserah
+global.mess = {
+    nsfw: '*Fitur NSFW tidak diaktifkan, chat admin grup supaya diaktifkan*_',
+    wait: '*Loading... Sabar Wibu*_',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',    
+}
+//=================================================//
+//Gausah Di Apa2 in
+global.f1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.f2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.f3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.f4 = 'application/zip'
+global.f5 = 'application/pdf'
+global.f6 = 'application/vnd.android.package-archive'
+//=================================================//
+global.limitawal = {
+    premium: "Infinity",
+    free: 5
+}
+//=================================================//
+//Terserah Kalau Paham 
+global.thumb = fs.readFileSync("./baseikal/image/hw.jpg")
+global.log0 = fs.readFileSync("./baseikal/image/haikal.jpg")
+//=================================================//
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
